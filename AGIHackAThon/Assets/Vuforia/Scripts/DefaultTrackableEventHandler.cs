a3dd9cda3487feb5345fa368257940f398a8f3de
Alexia Bernardo <ab4854@rit.edu>
2018-08-02 12:58:12 -0400

    Added replacement copy of defaulttrackableeventhandler.cs

--- AGIHackAThon/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ AGIHackAThon/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -1,6 +1,5 @@
 /*==============================================================================
 Copyright (c) 2017 PTC Inc. All Rights Reserved.
-
 Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
 All Rights Reserved.
 Confidential and Proprietary - Protected under copyright and other laws.
